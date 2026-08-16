303ea993a7d157d36a8fe92e2eef53eac82e5858
Craig Huckerby <craighckby@gmail.com>
2026-08-16 12:39:17 +1000

    [DARLEK CANN] Mutate src/plugins/knowledgeGraph.ts

--- src/plugins/knowledgeGraph.ts
+++ src/plugins/knowledgeGraph.ts
@@ -1,3 +1,10 @@
+/**
+ * DARLEK CANN ARCHITECTURAL HEADER
+ * File: src/plugins/knowledgeGraph.ts
+ * Role: Core system component participating in autonomous cognitive evolution cycles.
+ * Architecture: Type-safe modular unit with resilient state interfaces.
+ */
+
 import { pluginSystem, Plugin } from '../core/plugins/pluginSystem';
 import { vectorMemory } from '../core/memory/vectorStore';
 
