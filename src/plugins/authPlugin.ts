ac6fedbcc8a464991a9da04049030aa773cf0370
Craig Huckerby <craighckby@gmail.com>
2026-08-16 12:38:21 +1000

    [DARLEK CANN] Mutate src/plugins/authPlugin.ts

--- src/plugins/authPlugin.ts
+++ src/plugins/authPlugin.ts
@@ -1,3 +1,10 @@
+/**
+ * DARLEK CANN ARCHITECTURAL HEADER
+ * File: src/plugins/authPlugin.ts
+ * Role: Core system component participating in autonomous cognitive evolution cycles.
+ * Architecture: Type-safe modular unit with resilient state interfaces.
+ */
+
 import { pluginSystem, Plugin } from '../core/plugins/pluginSystem';
 import { keyVault } from '../core/auth/keyVault';
 
